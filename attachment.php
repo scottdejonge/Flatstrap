5d95c7377c274dc2ac6d916fed45f95c8e6ad08a
Scott de Jonge <dejonge.scott@gmail.com>
2013-07-22 11:42:38 +1000

    Added Attachment.php

---
+++ attachment.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<?php get_breadcrumbs(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="row">
+	<article id="post-<?php the_ID(); ?>" class="span8">
+		<div class="entry">
+			<h1><?php the_title(); ?></h1>
+			<?php $attachment = wp_get_attachment_image_src($post->id, 'full'); ?> 
+			<img src="<?php echo $attachment[0]; ?>" width="<?php echo $attachment[1]; ?>" height="<?php echo $attachment[2]; ?>">
+			<caption><?php echo get_the_excerpt(); ?></caption>
+			<?php the_content(); ?>
+		</div>
+	</article>
+</div>
+<?php endwhile; endif; ?>
+	
+<?php get_footer(); ?>
